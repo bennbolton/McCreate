57d9d3f70ec7c8a01ec6196992a78000fa3400d9
Ben <benbolton03@hotmail.com>
2025-01-26 16:01:06 +0000

    Added Peanut Oil Liquid Burning Capability

---
+++ overrides/kubejs/server_scripts/misc/liquidFuel.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(e => {
+    e.custom({   
+        "type": "liquidburner:liquidburning",   
+        "fluid": "garnished:peanut_oil",   
+        "burntime": "4000",      
+        "superheattime" : "2000"   
+    } )
+})
