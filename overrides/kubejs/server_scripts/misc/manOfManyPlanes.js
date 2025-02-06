f8550fb82a049f48bb244dc7b669bffab6065ce3
Ben <benbolton03@hotmail.com>
2025-02-06 22:48:10 +0000

    Various Last Minute Fixes

--- overrides/kubejs/server_scripts/misc/manOfManyPlanes.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes(e => {
-    e.remove({id: 'man_of_many_planes:economy_plane'})
-    e.remove({id: 'man_of_many_planes:scarlet_biplane'})
-})
--- overrides/kubejs/server_scripts/misc/recipeRemovals.js
+++ overrides/kubejs/server_scripts/misc/recipeRemovals.js
@@ -1,3 +1,6 @@
 ServerEvents.recipes(e => {
-    e.remove("petrolsparts:hydraulic_transmission")
+    e.remove({output: "petrolsparts:hydraulic_transmission"})
+    e.remove({id: 'man_of_many_planes:economy_plane'})
+    e.remove({id: 'man_of_many_planes:scarlet_biplane'})
+    e.remove({id: "petrolsparts:mechanical_crafting/hydraulic_transmission"})
 })
