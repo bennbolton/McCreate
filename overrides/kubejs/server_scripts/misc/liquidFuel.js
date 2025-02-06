cc99c9d8843be0e647bfe293ad8866953fda7e29
Ben <benbolton03@hotmail.com>
2025-02-06 21:41:02 +0000

    Changed Liquid Fuel Mod Peanut Files

--- overrides/kubejs/server_scripts/misc/liquidFuel.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes(e => {
-    e.custom({   
-        "type": "liquidburner:liquidburning",   
-        "fluid": "garnished:peanut_oil",  
-        "burntime": "0",
-        "superheattime": "3000"   
-    } )
-})
