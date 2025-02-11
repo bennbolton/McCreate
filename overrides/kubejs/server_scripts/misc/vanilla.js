e1583a41cadac8cfb1b63c51bbc40e9c13b7bbe7
Ben <benbolton03@hotmail.com>
2025-02-11 20:08:17 +0000

    Renewable Deepslate

--- overrides/kubejs/server_scripts/misc/vanilla.js
+++ overrides/kubejs/server_scripts/misc/vanilla.js
@@ -4,4 +4,6 @@ ServerEvents.recipes(e => {
     //should be updated to find all other wood types:
     e.recipes.create.item_application("garnsihed:stripped_nut_log", ["garnished:nut_log", "#minecraft:axes"])
     e.recipes.farmersdelight.cutting("garnished:nut_log", "#minecraft:axes", ["garnished:stripped_nut_log", "farmersdelight:tree_bark"])
+
+    e.recipes.create.compacting("3x minecraft:cobbled_deepslate", ["compressium:cobblestone_1", Fluid.of("minecraft:lava", 100)])
 })
