4880f01b0284125ee213fede22a5af252c735107
Ben <benbolton03@hotmail.com>
2025-02-21 13:26:00 +0000

    Removed Unnecessary Code For Removed Mods

--- overrides/kubejs/server_scripts/misc/recipeRemovals.js
+++ overrides/kubejs/server_scripts/misc/recipeRemovals.js
@@ -2,5 +2,4 @@ ServerEvents.recipes(e => {
     e.remove({output: "petrolsparts:hydraulic_transmission"})
     e.remove({id: 'man_of_many_planes:economy_plane'})
     e.remove({id: 'man_of_many_planes:scarlet_biplane'})
-    e.remove({id: "petrolsparts:mechanical_crafting/hydraulic_transmission"})
 })
